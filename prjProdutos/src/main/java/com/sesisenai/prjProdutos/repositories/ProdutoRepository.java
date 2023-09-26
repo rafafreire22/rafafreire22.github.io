a710a94d646c771ab78914463118abd6806f15e9
rafafreire22 <133990084+rafafreire22@users.noreply.github.com>
2023-09-26 08:07:32 -0300

    Delete prjProdutos directory

--- prjProdutos/src/main/java/com/sesisenai/prjProdutos/repositories/ProdutoRepository.java
+++
@@ -1,9 +0,0 @@
-package com.sesisenai.prjProdutos.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.sesisenai.prjProdutos.entities.Produto;
-
-public interface ProdutoRepository extends JpaRepository<Produto,Long>{
-
-}
