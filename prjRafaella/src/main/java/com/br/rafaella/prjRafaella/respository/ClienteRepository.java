5950fd493a77b9f7a8b5410269bab63fc6d7eb28
rafafreire22 <133990084+rafafreire22@users.noreply.github.com>
2023-09-26 08:07:46 -0300

    Delete prjRafaella directory

--- prjRafaella/src/main/java/com/br/rafaella/prjRafaella/respository/ClienteRepository.java
+++
@@ -1,9 +0,0 @@
-package com.br.rafaella.prjRafaella.respository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.br.rafaella.prjRafaella.entities.Cliente;
-
-public interface ClienteRepository extends JpaRepository<Cliente, Long> {
-
-}
